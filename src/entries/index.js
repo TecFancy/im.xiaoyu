d6a31222e7d51e0f0eb7d38664c8f9db23602a4f
smpower <rf.wangchn@gmail.com>
2018-09-05 22:57:04 +0800

    entry of index component that imports style

--- src/entries/index.js
+++ src/entries/index.js
@@ -3,5 +3,7 @@ import ReactDOM from 'react-dom';
 import {view as Index} from '../pages/index/';
 import registerServiceWorker from '../registerServiceWorker';
 
+import './style.css';
+
 ReactDOM.render(<Index />, document.getElementById('root'));
 registerServiceWorker();
