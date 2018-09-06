16b5171e28c70012d7dba0b49c7e614eaef464e8
smpower <bzsjxhywrf@outlook.com>
2018-09-06 09:13:30 +0800

    add tip of Header component

--- src/components/header/views/header.js
+++ src/components/header/views/header.js
@@ -3,7 +3,9 @@ import React from 'react';
 import './style.css';
 
 const Header = () => (
-  <div className="header">Header Component</div>
+  <div className="header">
+    Header (height: 36px)
+  </div>
 );
 
 export default Header;
