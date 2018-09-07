9c061e4c6f7d52c669d24ee0682641a52c9e900c
smpower <bzsjxhywrf@outlook.com>
2018-09-07 14:12:33 +0800

    update components: Article03 and Article04

---
+++ src/pages/article04/index.js
@@ -0,0 +1,3 @@
+import view from './views/article04.js';
+
+export {view};
---
+++ src/pages/article04/views/article04.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class Article04 extends React.Component {
+  render() {
+    return (
+      <div>文章页面: Article04</div>
+    );
+  }
+}
+
+export default Article04;
