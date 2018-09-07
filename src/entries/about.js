b256b6f08d7044409b57cc859347ab4f6d314cca
smpower <bzsjxhywrf@outlook.com>
2018-09-07 10:49:36 +0800

    About and Topics component import common style

--- src/entries/about.js
+++ src/entries/about.js
@@ -3,5 +3,7 @@ import ReactDOM from 'react-dom';
 import {view as About} from '../pages/about/';
 import registerServiceWorker from '../registerServiceWorker';
 
+import './style.css';
+
 ReactDOM.render(<About />, document.getElementById('root'));
 registerServiceWorker();
