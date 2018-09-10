74dd9abdbb00c4694e7a38eaadcae1f36272968a
smpower <bzsjxhywrf@outlook.com>
2018-09-10 14:44:23 +0800

    add three button

--- src/pages/index/reducer.js
+++ src/pages/index/reducer.js
@@ -98,7 +98,7 @@ const classifies = [
     },
     {
       type: 'article',
-      title: '文章页面01',
+      title: '婚姻破裂',
       href: './article01/',
       nav: [
 	{
@@ -147,7 +147,7 @@ const classifies = [
     },
     {
       type: 'article',
-      title: '文章页面02',
+      title: '异地恋情',
       href: './article02/',
       nav: [
 	{
@@ -196,7 +196,7 @@ const classifies = [
     },
     {
       type: 'article',
-      title: '图标大小',
+      title: '性格不合',
       href: './article03/',
       nav: [
 	{
@@ -245,7 +245,154 @@ const classifies = [
     },
     {
       type: 'article',
-      title: '80 * 80',
+      title: '恋人劈腿',
+      href: './article04/',
+      nav: [
+	{
+	  text: '分类一',
+	  url: '#/nav'
+	},
+	{
+	  text: '分类二',
+	  url: '#/nav'
+	},
+	{
+	  text: '分类三',
+	  url: '#/nav'
+	},
+	{
+	  text: '分类四',
+	  url: '#/nav'
+	}
+      ],
+      list: [
+	{
+	  url: '#/',
+	  title: '学会这几招教你分分钟挽回女友，还不赶紧学起来！',
+	  description: '挽回女友超简单，会这几招就够咯',
+	  image: images.articleImages.articleImage01
+	},
+	{
+	  url: '#/',
+	  title: '今天我们来谈论一下伦理问题',
+	  description: '针对当今社会愈发频繁发生的伦理现象深度解读',
+	  image: images.articleImages.articleImage01
+	},
+	{
+	  url: '#/',
+	  title: '设计师注意',
+	  description: '右侧的配图大小为 180 * 180',
+	  image: images.articleImages.articleImage01
+	},
+	{
+	  url: '#/',
+	  title: '右侧的配图大小为 180 * 180',
+	  description: '文章简短描述',
+	  image: images.articleImages.articleImage01
+	}
+      ]
+    },
+    {
+      type: 'article',
+      title: '感情淡化',
+      href: './article04/',
+      nav: [
+	{
+	  text: '分类一',
+	  url: '#/nav'
+	},
+	{
+	  text: '分类二',
+	  url: '#/nav'
+	},
+	{
+	  text: '分类三',
+	  url: '#/nav'
+	},
+	{
+	  text: '分类四',
+	  url: '#/nav'
+	}
+      ],
+      list: [
+	{
+	  url: '#/',
+	  title: '学会这几招教你分分钟挽回女友，还不赶紧学起来！',
+	  description: '挽回女友超简单，会这几招就够咯',
+	  image: images.articleImages.articleImage01
+	},
+	{
+	  url: '#/',
+	  title: '今天我们来谈论一下伦理问题',
+	  description: '针对当今社会愈发频繁发生的伦理现象深度解读',
+	  image: images.articleImages.articleImage01
+	},
+	{
+	  url: '#/',
+	  title: '设计师注意',
+	  description: '右侧的配图大小为 180 * 180',
+	  image: images.articleImages.articleImage01
+	},
+	{
+	  url: '#/',
+	  title: '右侧的配图大小为 180 * 180',
+	  description: '文章简短描述',
+	  image: images.articleImages.articleImage01
+	}
+      ]
+    },
+    {
+      type: 'article',
+      title: '挽回干货',
+      href: './article04/',
+      nav: [
+	{
+	  text: '分类一',
+	  url: '#/nav'
+	},
+	{
+	  text: '分类二',
+	  url: '#/nav'
+	},
+	{
+	  text: '分类三',
+	  url: '#/nav'
+	},
+	{
+	  text: '分类四',
+	  url: '#/nav'
+	}
+      ],
+      list: [
+	{
+	  url: '#/',
+	  title: '学会这几招教你分分钟挽回女友，还不赶紧学起来！',
+	  description: '挽回女友超简单，会这几招就够咯',
+	  image: images.articleImages.articleImage01
+	},
+	{
+	  url: '#/',
+	  title: '今天我们来谈论一下伦理问题',
+	  description: '针对当今社会愈发频繁发生的伦理现象深度解读',
+	  image: images.articleImages.articleImage01
+	},
+	{
+	  url: '#/',
+	  title: '设计师注意',
+	  description: '右侧的配图大小为 180 * 180',
+	  image: images.articleImages.articleImage01
+	},
+	{
+	  url: '#/',
+	  title: '右侧的配图大小为 180 * 180',
+	  description: '文章简短描述',
+	  image: images.articleImages.articleImage01
+	}
+      ]
+    },
+    {
+      type: 'article',
+      title: '矛盾积累',
       href: './article04/',
       nav: [
 	{
