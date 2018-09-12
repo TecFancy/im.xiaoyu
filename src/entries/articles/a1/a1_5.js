134d159386c57ebcbffff0219f3a4ef48f9b89cf
smpower <bzsjxhywrf@outlook.com>
2018-09-12 17:16:53 +0800

    优化文章页面

--- src/entries/articles/a1/a1_5.js
+++ src/entries/articles/a1/a1_5.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {Provider} from 'react-redux';
+import store from '../../../Store.js';
 import {view as Article} from '../../../pages/articles/a1/a1_5/';
 import registerServiceWorker from '../../../registerServiceWorker';
 
 import '../../style.css';
 import '../style.css';
 
-ReactDOM.render(<Article />, document.getElementById('root'));
+ReactDOM.render(
+  <Provider store={store}>
+    <Article />
+  </Provider>,
+  document.getElementById('root')
+);
 registerServiceWorker();
--- src/pages/articles/a1/a1_1/views/a1_1.js
+++ src/pages/articles/a1/a1_1/views/a1_1.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+import {view as Header} from '../../../../../components/header/';
+import {view as ArticleBreadCrumb} from '../../../../../components/articleBreadCrumb/';
 import {view as ArticleOverflow} from '../../../../../components/articleOverflow/';
 import {view as Footer} from '../../../../../components/footer/';
 import markdown from '../../../../../utilities/markdown.js';
@@ -43,6 +45,8 @@ class A1_1 extends React.Component {
     const hideState = this.state.articleOverflow.hide;
     return (
       <div className="article">
+	<Header home={true}/>
+	<ArticleBreadCrumb text="婚姻破裂" url="../../../classify01/"/>
 	<div className={`${hideState ? 'article-content show' : 'article-content'}`}>
 	  <div className="markdown-body" dangerouslySetInnerHTML={{__html: result}}></div>
 	  <ArticleOverflow />
