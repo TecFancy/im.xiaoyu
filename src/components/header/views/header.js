a5b83c0fe040d187419bce117dfe74ed0a858036
smpower <bzsjxhywrf@outlook.com>
2018-09-13 16:53:20 +0800

    update Header component

--- src/components/header/views/header.js
+++ src/components/header/views/header.js
@@ -8,6 +8,7 @@ class Header extends React.Component {
       return (
 	<div className="header home-header">
 	  <span className="header-logo"></span>
+	  <span className="header-text">专注情感 为爱而生</span>
 	</div>
       );
     }
