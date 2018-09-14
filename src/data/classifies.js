62d40b402b0b6a8420b442fba4eddb88e0db7371
smpower <bzsjxhywrf@outlook.com>
2018-09-14 17:04:18 +0800

    update description of each article

--- src/data/classifies.js
+++ src/data/classifies.js
@@ -27,31 +27,31 @@ const classifies = [
 	{
 	  url: 'articles/a1/a1_1/',
 	  title: '设计师注意 - 男人你听我说，你所追求的是“保姆式妻子”和“守寡式婚姻”',
-	  description: '右侧的配图大小为 80 * 80 - 简短描述...',
+	  description: '我的妻子是个很美丽的女人，但是结婚两年了，她还不想要孩子。',
 	  image: images.articleImages.articleImage001
 	},
 	{
 	  url: 'articles/a1/a1_2/',
 	  title: '傻女人，你真以为用孩子就能留住婚姻？！',
-	  description: '简短描述...',
+	  description: '早在2016年，朋友的老公出轨了公司助理。去年，朋友为了挽留老公，又养了二胎，本以为两人找回了恩爱，谁知前几天她又找我倾诉，抱怨上天的不公平。',
 	  image: images.articleImages.articleImage002
 	},
 	{
 	  url: 'articles/a1/a1_3/',
 	  title: '老公爱聊骚，我除了忍别无他法',
-	  description: '简短描述...',
+	  description: '今年是我和老公在一起的第七年，我们有一个两岁的孩子。',
 	  image: images.articleImages.articleImage004
 	},
 	{
 	  url: 'articles/a1/a1_4/',
 	  title: '原配实力虐小三，男人想复婚只能靠跪舔吗？',
-	  description: '文章简短描述...',
+	  description: '当年我也是因为一念之差走错了路，当初妻子和我结婚的时候，我一穷二白什么都没有，她一直做我背后的女人鼓励我，后来开始创业了，用五年的时间赚了很多钱，换了大房子，也要了第二胎，本来生活小康，一双儿女，家庭也是很幸福的。',
 	  image: images.articleImages.articleImage006
 	},
 	{
 	  url: 'articles/a1/a1_5/',
 	  title: '你的面子值钱，爱人的尊严就分文不值吗？',
-	  description: '简短描述...',
+	  description: '为什么有些人的婚姻可以天长地久，而有些人的婚姻却千疮百孔？',
 	  image: images.articleImages.articleImage007
 	},
       ]
@@ -82,31 +82,31 @@ const classifies = [
 	{
 	  url: 'articles/a2/a2_1/',
 	  title: '异地恋分手：别把原因一味归咎给距离！',
-	  description: '简短描述...',
+	  description: '学员英赫（化名）和女友银河（化名）感情基础其实是很坚实的，他们俩相识于大学校园，大一就确立了恋爱关系，大学这四年，他们正经谈了好一段甜蜜纯粹的恋爱。',
 	  image: images.articleImages.articleImage008
 	},
 	{
 	  url: 'articles/a2/a2_2/',
 	  title: '两年异地恋，三个月见一面，找个小姐怎么了？',
-	  description: '简短描述...',
+	  description: '异地的情侣总有很多不足为外人道的问题，这不小鱼情感的学员章明（化名）就遇到了这样的问题：',
 	  image: images.articleImages.articleImage010
 	},
 	{
 	  url: 'articles/a2/a2_3/',
 	  title: '异地恋是真的不容易，你看我这满满的火车票就知道了',
-	  description: '简短描述...',
+	  description: '从前一天一个电话，仿佛有说不完的话，开心的不开心的事都想和对方分享。',
 	  image: images.articleImages.articleImage012
 	},
 	{
 	  url: 'articles/a2/a2_4/',
 	  title: '一段被包养的异地恋情，还有机会挽回吗？',
-	  description: '简短描述...',
+	  description: '陈熙是我的一个朋友，也是我们小鱼情感的一位学员，最近他遇到了情感问题，大半夜和我一顿倾诉，我隐约记得两个人应该才谈了不到一年的时间，怎么就要闹分手了呢？',
 	  image: images.articleImages.articleImage013
 	},
 	{
 	  url: 'articles/a2/a2_5/',
 	  title: '异地恋，到底有多煎熬？网友：累了，真的累了',
-	  description: '简短描述...',
+	  description: '明明谈着两个人的恋爱，却不得不过着单身一样的生活。我们常说，异地恋是最难的恋爱，但是熬过了就是一辈子。异地恋最煎熬的是什么？做什么都是一个人，累了，真的累了！',
 	  image: images.articleImages.articleImage014
 	}
       ]
@@ -137,31 +137,31 @@ const classifies = [
 	{
 	  url: 'articles/a3/a3_1/',
 	  title: '面对“性格不合 对你没感觉”的分手，我该如何挽回？',
-	  description: '简短描述...',
+	  description: '“我对你没感觉，咱们性格不合适”这句话几乎是情侣分手时最常见的理由。',
 	  image: images.articleImages.articleImage015
 	},
 	{
 	  url: 'articles/a3/a3_2/',
 	  title: '优柔寡断的男人如何改善性格成功挽回？',
-	  description: '简短描述...',
+	  description: '很多女性抱怨现在的男人们都变得过度“温柔”了，遇到事情没有男人样子。',
 	  image: images.articleImages.articleImage016
 	},
 	{
 	  url: 'articles/a3/a3_3/',
 	  title: '“分手了，都怪我性格太内向了！”',
-	  description: '简短描述...',
+	  description: '说起人的性格，首先浮现在你脑海里的，也无非就是“内向”、“外向”两种。不同的性格有各自的优势劣势，但性格本身没有高下之别。',
 	  image: images.articleImages.articleImage017
 	},
 	{
 	  url: 'articles/a3/a3_4/',
 	  title: '别真信了“性格不合”的分手理由，你必须要看清这背后隐藏的问题',
-	  description: '简短描述...',
+	  description: '这可能是最烂大街的分手理由了，许多人在听到这样的话以后也会彻底丧失挽回的信心，觉得这种深层次的问题是没办法改变的。可他们却忘了应该跳出这句抽象的理由，去真正找出两人的关系当中存在的问题。',
 	  image: images.articleImages.articleImage018
 	},
 	{
 	  url: 'articles/a3/a3_5/',
 	  title: '性格偏执型恋人追求爱情完美 最终断送幸福',
-	  description: '简短描述...',
+	  description: '董青（女化名）是我在小鱼情感最近接触的学员，正如微信中所言，她就是爱情中的“完美主义者”。董青对待爱情有自己的一套标准，既然两个人自愿选择在一起，那么为了心中完美的爱情，严格要求男友“服从”，特别是外表形象和交友方面。',
 	  image: images.articleImages.articleImage019
 	}
       ]
@@ -192,31 +192,31 @@ const classifies = [
 	{
 	  url: 'articles/a4/a4_1/',
 	  title: '女朋友出轨了，还有必要挽回吗？',
-	  description: '简短描述...',
+	  description: '大家好，我们是小鱼情感。专注情感，为爱而生。小鱼情感，致力于为大家提供挽回爱情、挽救婚姻、分离小三、订制爱情等两性情感服务。',
 	  image: images.articleImages.articleImage020
 	},
 	{
 	  url: 'articles/a4/a4_2/',
 	  title: '只有上了床才叫出轨吗？这里有你想要的答案',
-	  description: '简短描述...',
+	  description: '大家好，我们是小鱼情感。专注情感，为爱而生。小鱼情感，致力于为大家提供挽回爱情、挽救婚姻、分离小三、订制爱情等两性情感服务。',
 	  image: images.articleImages.articleImage022
 	},
 	{
 	  url: 'articles/a4/a4_3/',
 	  title: '出轨的男人如何挽回爱情？',
-	  description: '简短描述...',
+	  description: '大家好，我们是小鱼情感。专注情感，为爱而生。小鱼情感，致力于为大家提供挽回爱情、挽救婚姻、分离小三、订制爱情等两性情感服务。',
 	  image: images.articleImages.articleImage023
 	},
 	{
 	  url: 'articles/a4/a4_4/',
 	  title: '游走于两个女人间的劈腿男，竟然是不满足的受害者？',
-	  description: '简短描述...',
+	  description: '提到恋爱关系中的劈腿，每个人脑海里浮现的，都是背叛感情的一方太渣，然而奇怪的是，主动劈腿的一方却以受害者形象自居，认为自己之所以会爱上伴侣以外的人，都是伴侣不够好，逼的自己另寻新欢，对此大家怎么看？',
 	  image: images.articleImages.articleImage024
 	},
 	{
 	  url: 'articles/a4/a4_5/',
 	  title: '精神出轨，我还能挽回吗？',
-	  description: '简短描述...',
+	  description: '“饮食男女，人之大欲存焉”，人这一生离不开两件事：饮食、男女关系。提到男女关系，就不得不提男人出轨的事情，出轨也分两种，肉体出轨和精神出轨。',
 	  image: images.articleImages.articleImage025
 	}
       ]
@@ -247,31 +247,31 @@ const classifies = [
 	{
 	  url: 'articles/a5/a5_1/',
 	  title: '新鲜感褪去后，爱情该何去何从？',
-	  description: '简短描述...',
+	  description: '当爱情褪去最初火热的温度，逐渐向平淡中走去，爱情究竟是走向了细水长流、返璞归真，还是“不在沉默中爆发，就在沉默中灭亡”？',
 	  image: images.articleImages.articleImage026
 	},
 	{
 	  url: 'articles/a5/a5_2/',
 	  title: '情侣间新鲜感殆尽，除了分手我们还能做什么？',
-	  description: '简短描述...',
+	  description: '爱情如果能够始终维持在最初的甜蜜该多好，那就该省却了很多日后的争吵和烦恼。',
 	  image: images.articleImages.articleImage031
 	},
 	{
 	  url: 'articles/a5/a5_3/',
 	  title: '七年感情一朝散，我们的爱情“过期”了',
-	  description: '简短描述...',
+	  description: '“我们在一起七年了，除了没领证，我们就和夫妻一样，可如今她说走就走了。”小鱼情感新来的学员刘天（化名）为我们讲述了他和女友的情感经历。',
 	  image: images.articleImages.articleImage032
 	},
 	{
 	  url: 'articles/a5/a5_4/',
 	  title: '爱情没有新鲜感 恩爱情侣告诉你需要三种“距离感”来拯救',
-	  description: '简短描述...',
+	  description: '谈了3年的希文（男化名）和郑悦（女化名），分手了。',
 	  image: images.articleImages.articleImage033
 	},
 	{
 	  url: 'articles/a5/a5_5/',
 	  title: '面对新鲜感缺失该怎么办？',
-	  description: '简短描述...',
+	  description: '从当初的一见钟情，到现在的相伴携手，李超跟女友已经一起走过三个年头了。按理说，历经了三年的磨合，两人之间应该越来越契合，可事实上，最近一年里两人之间的矛盾和摩擦越来越多，一丁点风吹草动都会引发，两人之间的争吵。',
 	  image: images.articleImages.articleImage034
 	}
       ]
@@ -302,31 +302,31 @@ const classifies = [
 	{
 	  url: 'articles/a6/a6_1/',
 	  title: '情侣分手后复合的概率是82%，你为什么失败了？',
-	  description: '简短描述...',
+	  description: '心理动机会影响人的行为模式，很多人在自行挽回的途中会迷失方向，以至于忘记了自己最初挽回的原因，由此就产生了很多错误的挽回方式。比如无下限的纠缠，讨好和自残等，没想到对方非但不领情，反而在你的仓促挽回中毫不留情的拉黑你，选择逃离你。',
 	  image: images.articleImages.articleImage035
 	},
 	{
 	  url: 'articles/a6/a6_2/',
 	  title: '当爱情被直男癌症状击垮 我该如何挽回？',
-	  description: '简短描述...',
+	  description: '在当今飞速发展的社会中，不断出现的各种机遇在带给人们更多选择机会与物质财富的同时，也将一些难以面对的问题抛给了所有人。',
 	  image: images.articleImages.articleImage036
 	},
 	{
 	  url: 'articles/a6/a6_3/',
 	  title: '当心了！男人只想玩暧昧的几种表现，女生必看！',
-	  description: '简短描述...',
+	  description: '昨天有个姑娘找到我，诉说“暧昧”带来的烦恼：',
 	  image: images.articleImages.articleImage037
 	},
 	{
 	  url: 'articles/a6/a6_4/',
 	  title: '因“依恋损伤”而分手，如何快速走出心理创伤？',
-	  description: '简短描述...',
+	  description: '在解释这个定义之前，我们先了解一下四种成人依恋的模型，安全型、痴迷型、疏离型和恐惧型：',
 	  image: images.articleImages.articleImage038
 	},
 	{
 	  url: 'articles/a6/a6_5/',
 	  title: '谈了100次恋爱屡战屡败？因为你陷入了“低自尊型恋爱”',
-	  description: '简短描述...',
+	  description: '在聊今天的话题之前，我们先来看一个截图，对方是一位来小鱼情感咨询情感问题的朋友：',
 	  image: images.articleImages.articleImage039
 	}
       ]
@@ -357,31 +357,31 @@ const classifies = [
 	{
 	  url: 'articles/a7/a7_1/',
 	  title: '冷战，只会让矛盾长期积压',
-	  description: '简短描述...',
+	  description: '大家好，我们是小鱼情感。专注情感，为爱而生。小鱼情感，致力于为大家提供挽回爱情、挽救婚姻、分离小三、订制爱情等两性情感服务。',
 	  image: images.articleImages.articleImage040
 	},
 	{
 	  url: 'articles/a7/a7_2/',
 	  title: '小矛盾堆积成大问题，分手来的猝不及防',
-	  description: '简短描述...',
+	  description: '刘亮（化名）和女友小夏（化名）在一起五年，在对彼此越来越了解的同时，两人之间的矛盾也不可避免的多了起来。',
 	  image: images.articleImages.articleImage042
 	},
 	{
 	  url: 'articles/a7/a7_3/',
 	  title: '“破窗效应”告诉你：小事背后隐藏分手本质',
-	  description: '简短描述...',
+	  description: '在小鱼情感从事挽回行业多年，接触到的学员中，有60%的人是因为小事爆发争吵而分手，很多学员也疑惑为什么伴侣会因为鸡毛蒜皮的小事就毅然决然提出分手。',
 	  image: images.articleImages.articleImage043
 	},
 	{
 	  url: 'articles/a7/a7_4/',
 	  title: '小矛盾膨胀激化，最终引发分手，你做错了什么？',
-	  description: '简短描述...',
+	  description: '我叫钟林（化名），今年32岁，私企中层，收入中等。我女朋友的职业是小学语文老师，今年29岁。',
 	  image: images.articleImages.articleImage044
 	},
 	{
 	  url: 'articles/a7/a7_5/',
 	  title: '因为小事而分手？那是因为你不知道她忍了多久',
-	  description: '简短描述...',
+	  description: '不少小鱼情感的学员在我们问及分手原因的时候，都说是女友太作，因为鸡毛蒜皮的小事就提了分手，其实这件小事的背后隐藏着她太久的隐忍和失望，看过了下面这两位学员的案例，可能你就对“那件分手的小事”有了新的认识。',
 	  image: images.articleImages.articleImage045
 	}
       ]
