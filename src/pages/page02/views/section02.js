595ac669adce1b7196dac0f4c624f05753bf0851
rf <rf@debian.localhost>
2018-09-19 16:08:44 +0800

    page02 页面添加“小鱼情感首席导师”模块

---
+++ src/pages/page02/views/section02.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import SectionTitle from './title.js';
+
+const Section02 = () => (
+  <div className="page02-section02">
+    <SectionTitle title="情感导师" />
+  </div>
+);
+
+export default Section02;
---
+++ src/pages/page02/views/title.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Title = ({title}) => (
+  <div className="page02-title">
+    <i className="page02-title-icon_left"></i>
+    <h3 className="page02-title-text">{title}</h3>
+    <i className="page02-title-icon_right"></i>
+  </div>
+);
+
+export default Title;
