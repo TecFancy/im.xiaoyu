4df7719886f0439652c066909c4abe3f3872c8a1
smpower <rf.wangchn@gmail.com>
2018-09-23 09:31:14 +0800

    page02 页面添加 banner 图片

--- src/pages/page02/views/page02.js
+++ src/pages/page02/views/page02.js
@@ -3,9 +3,12 @@ import {view as Header} from '../../../components/header/';
 
 import './style.css';
 
+import bannerImage from './images/banner.png';
+
 const Page02 = () => (
   <div className="page02">
     <Header home={false} title="服务范围"/>
+    <img className="page02-banner" src={bannerImage} alt="服务范围_权威专家" />
   </div>
 );
 
