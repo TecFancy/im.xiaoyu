19a66d4ff2af02ae6f9a4114de3f9a5bc91b9b35
smpower <rf.wangchn@gmail.com>
2018-09-23 11:22:51 +0800

    page02 页面添加“服务范围”模块

--- src/pages/page02/views/page02.js
+++ src/pages/page02/views/page02.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import {view as Header} from '../../../components/header/';
+import Service from './service.js';
 
 import './style.css';
 
@@ -9,6 +10,7 @@ const Page02 = () => (
   <div className="page02">
     <Header home={false} title="服务范围"/>
     <img className="page02-banner" src={bannerImage} alt="服务范围_权威专家" />
+    <Service />
   </div>
 );
 
