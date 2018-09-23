3af00243867440ca134b841dd5b207c8a3ead3a0
smpower <rf.wangchn@gmail.com>
2018-09-23 15:08:35 +0800

    page02 页面添加“服务流程”模块

--- src/pages/page02/views/page02.js
+++ src/pages/page02/views/page02.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import {view as Header} from '../../../components/header/';
 import Service from './service.js';
+import Circuit from './circuit.js';
 
 import './style.css';
 
@@ -11,6 +12,7 @@ const Page02 = () => (
     <Header home={false} title="服务范围"/>
     <img className="page02-banner" src={bannerImage} alt="服务范围_权威专家" />
     <Service />
+    <Circuit />
   </div>
 );
 
