4f2c84c545996ae9b619714ee2b8535ab7b32602
smpower <rf.wangchn@gmail.com>
2018-10-17 18:37:52 +0800

    更新权威团队页面情感导师模块导师头像和对应描述

--- src/pages/page03/views/section02.js
+++ src/pages/page03/views/section02.js
@@ -9,24 +9,19 @@ import mentorImage05 from './images/avatar_mentor05.png';
 
 const mentorInfos = [
   {
-    mentorImage: mentorImage01,
-    mentorName: '皓霖导师',
-    des: '长期关系维护专家'
-  },
-  {
     mentorImage: mentorImage02,
-    mentorName: '孑然导师',
-    des: '首席挽回专家'
+    mentorName: '木子导师',
+    des: '国家首批挽回专家'
   },
   {
     mentorImage: mentorImage03,
-    mentorName: '谧沙导师',
-    des: '国家婚姻情感咨询师'
+    mentorName: '文琪导师',
+    des: '高级两性心理学专家'
   },
   {
     mentorImage: mentorImage04,
-    mentorName: '文心导师',
-    des: '狙击挽回实战专家'
+    mentorName: '泊乔导师',
+    des: '国家首批情感挽回专家'
   },
   {
     mentorImage: mentorImage05,
